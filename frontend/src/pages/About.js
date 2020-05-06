63ee296d927fa827de99e04476657eca1acd1764
thesayedfahad <fsayed@my.bcit.ca>
2020-05-05 23:29:35 -0700

    edited about us page

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -13,7 +13,57 @@ class About extends Component {
     render() {
         return (
             <div>
-
+                <h1 class="text-center page-header">About Us</h1>
+                <div class="mx-auto card w-75">
+                    <div>
+                    <h5 class="card-header">Alireza Kakan</h5>
+                    </div>
+                    <div class="card-body">
+                        <blockquote class ="blockquote mb-0">
+                            <p>I Am ........ sjndfisuafh usjfihaushf nfuie difhsdjf cushf 
+                                sdfbuyewufdh 
+                            </p>
+                            <footer class="blockquote-footer">Alireza Kakan</footer>
+                        </blockquote>
+                        
+                    </div>
+                    <div>
+                    <h5 class="card-header">Edgar</h5>
+                    </div>
+                    <div class="card-body">
+                        <blockquote class ="blockquote mb-0">
+                            <p>I Am ........ sjndfisuafh usjfihaushf nfuie difhsdjf cushf 
+                                sdfbuyewufdh 
+                            </p>
+                            <footer class="blockquote-footer">Edgar</footer>
+                        </blockquote>
+                        
+                    </div>
+                    <div>
+                    <h5 class="card-header">Tommy</h5>
+                    </div>
+                    <div class="card-body">
+                        <blockquote class ="blockquote mb-0">
+                            <p>I Am ........ sjndfisuafh usjfihaushf nfuie difhsdjf cushf 
+                                sdfbuyewufdh 
+                            </p>
+                            <footer class="blockquote-footer">Tommy</footer>
+                        </blockquote>
+                        
+                    </div>
+                    <div>
+                    <h5 class="card-header">Sayed Fahad</h5>
+                    </div>
+                    <div class="card-body">
+                        <blockquote class ="blockquote mb-0">
+                            <p>I Am ........ sjndfisuafh usjfihaushf nfuie difhsdjf cushf 
+                                sdfbuyewufdh 
+                            </p>
+                            <footer class="blockquote-footer">Sayed Fahad</footer>
+                        </blockquote>
+                        
+                    </div>
+                </div>
             </div>
         );
     }
