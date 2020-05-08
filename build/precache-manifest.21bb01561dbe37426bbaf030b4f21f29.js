05b884309d4b635471bef56d8c36bcdedc7e88f5
Edgar Coime <e.coimeii@gmail.com>
2020-05-08 15:46:26 -0700

    expiremental backend hosting

---
+++ build/precache-manifest.21bb01561dbe37426bbaf030b4f21f29.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "715385a17f5e1faba2ae90c212bd4edb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "954cfafe70f5c27125fb",
+    "url": "/static/css/2.742f74b6.chunk.css"
+  },
+  {
+    "revision": "bece71379ac47174bf41",
+    "url": "/static/css/main.8808f73c.chunk.css"
+  },
+  {
+    "revision": "954cfafe70f5c27125fb",
+    "url": "/static/js/2.929994e9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.929994e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bece71379ac47174bf41",
+    "url": "/static/js/main.6febbe53.chunk.js"
+  },
+  {
+    "revision": "725bae86733c0433390f",
+    "url": "/static/js/runtime-main.ffcfc917.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "40f99bd094ebcdd3749c945d44ce63e0",
+    "url": "/static/media/logo_transparent.40f99bd0.png"
+  }
+]);
--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 // ESM syntax is supported.
-export {}
+export {};
 
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import favicon from "express-favicon";
+import path from "path";
 const dotenv = require("dotenv").config();
 
 // Routes
-import { 
-  userRouter, 
-  postRouter, 
+import {
+  userRouter,
+  postRouter,
   authRouter,
   commentRouter,
-  covidPostRouter
+  covidPostRouter,
 } from "./routes/index";
 
 const app = express();
@@ -23,15 +25,28 @@ app.disable("x-powered-by");
 // Middleware to allow Cross origin point, parsing JSON, and body
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.json());
+app.use(favicon(__dirname + "/build/favicon.ico"));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "build")));
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, 
-  { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
-);
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 const connection = mongoose.connection;
-connection.once('open', () => {
+connection.once("open", () => {
   console.log("MongoDB database connection established succesfully");
+});
+
+app.get("/ping", function (req, res) {
+  return res.send("pong");
+});
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 // User Routes
@@ -43,9 +58,8 @@ apiRouter.use("/auth", authRouter);
 apiRouter.use("/comments", commentRouter);
 apiRouter.use("/covid", covidPostRouter);
 
-
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`)
-})
+  console.log(`Server is running on port: ${port}`);
+});
