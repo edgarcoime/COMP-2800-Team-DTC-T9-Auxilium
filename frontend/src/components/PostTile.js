a2ac0fa4c32eff4b2c617e39e10f273490728186
Edgar Coime <e.coimeii@gmail.com>
2020-05-08 01:13:56 -0700

    added an "updated" and "created" time stamp per post

--- frontend/src/components/PostTile.js
+++ frontend/src/components/PostTile.js
@@ -1,12 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
-import {
-  Row,
-  Col,
-  Card,
-  CardBody,
-  CardTitle,
-} from "reactstrap";
+import { Row, Col, Card, CardBody, CardTitle } from "reactstrap";
 import LikeComment from "./LikeComment";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.min.css";
@@ -19,19 +13,39 @@ class PostTile extends Component {
     };
   }
 
-  whenPosted(createdAt) {
+  postCreated = (createdAt) => {
+    let createdWhen = "";
     const created = Date.parse(createdAt);
     let now = Date.now();
     const differenceInMilliSecond = now - created;
     const h = differenceInMilliSecond / 1000 / 60 / 60;
-    // console.log(h);
     if (h >= 24) {
-      return Math.trunc(h / 24) + "d ago";
+      createdWhen = Math.trunc(h / 24) + "d ago";
     } else if (h < 1) {
-      return Math.trunc(h * 60) + "m ago";
+      createdWhen = Math.trunc(h * 60) + "m ago";
+    } else {
+      createdWhen = Math.trunc(h) + "h ago";
     }
-    return Math.trunc(h) + "h ago";
-  }
+
+    return `Created: ${createdWhen}`;
+  };
+
+  postUpdated = (updatedAt) => {
+    let updatedWhen = "";
+    const created = Date.parse(updatedAt);
+    let now = Date.now();
+    const differenceInMilliSecond = now - created;
+    const h = differenceInMilliSecond / 1000 / 60 / 60;
+    if (h >= 24) {
+      updatedWhen = Math.trunc(h / 24) + "d ago";
+    } else if (h < 1) {
+      updatedWhen = Math.trunc(h * 60) + "m ago";
+    } else {
+      updatedWhen = Math.trunc(h) + "h ago";
+    }
+
+    return `Updated: ${updatedWhen}`;
+  };
 
   componentDidMount() {
     axios.get("/api/posts/getall").then((res) => {
@@ -55,10 +69,13 @@ class PostTile extends Component {
                         <strong>{post.owner}</strong>
                       </p>
                     </Col>
+                    {/* {console.log(Date.parse(post.createdAt))} */}
                     <Col className="col-4 col-sm-2">
-                      {/* {console.log(Date.parse(post.createdAt))} */}
                       <span className="float-right">
-                        {this.whenPosted(post.createdAt)}
+                        {this.postCreated(post.createdAt)}
+                      </span>
+                      <span className="float-right">
+                        {this.postUpdated(post.updatedAt)}
                       </span>
                     </Col>
                   </Row>
