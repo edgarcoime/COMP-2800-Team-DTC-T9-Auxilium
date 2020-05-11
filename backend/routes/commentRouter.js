39c9b4a0954044b584581143c13843f7888f1d01
Tommy-81321 <56455799+Tommy-81321@users.noreply.github.com>
2020-05-11 00:45:13 -0700

    add likeRouter

--- backend/routes/commentRouter.js
+++ backend/routes/commentRouter.js
@@ -1,45 +1,68 @@
 import express from "express";
 
-// Item model to find data in DB
-import Comment from "../models/Comment.model";
-
 // Auth middleware
 import auth from "../middleware/auth.middleware";
 
+import Post from "../models/Post.model";
+
 const commentRouter = express.Router();
 
-// @route     GET api/comments/:postId
-// @desc      Get all comments under a post by post ID 
-// @access    Public
-commentRouter.get("/", async (req, res) => {
-  try {
-    const response = await Comment.find().sort({ date: -1 });
-    res.json(response);
+
+// @route     POST api/comment
+// @desc      Post comment
+// @access    Private (implement auth later)
+commentRouter.post("/", auth,async (req, res) => {
+  const { text, owner,ownerId, postId } = req.body;
+  try{
+  const post = await Post.findByIdAndUpdate(postId,{$push: {
+          comments: { text: text, owner:owner, ownerId:ownerId }
+        }
+    }, {
+        new: true
+    })
+      .exec((err, result) => {
+      if (err) {
+        return res.status(422).json({ error: err, msg: "Please provide correct owner name, owerId and postid"})
+      } else {
+          res.json(result)
+      }
+  })
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 });
 
-// @route     Post api/comments/:postId
-// @desc      Post a comments under a post by post ID 
-// @access    Public
-commentRouter.post("/:postId", async (req, res) => {
-  res.json({ msg: "This is where the single post located" })
-});
 
-// @route     GET api/comments/:postId/:commentId
-// @desc      GET a comments under a post by post ID and comment ID
-// @access    Public
-commentRouter.get("/:postId/:commentId", async (req, res) => {
-  res.json({ msg: "This is where a comment under a post id" })
-});
 
-// @route     GET api/comments/:postId/:commentId
-// @desc      Get all comments under a post by post ID and comment ID
+// @route     Post api/comment/:commentId
+// @desc      Delete a comments under a post by text,owner,ownerId
 // @access    Public
-commentRouter.delete("/:postId/:commentId", async (req, res) => {
-  res.json({ msg: "Delete a comment" })
-});
- 
+commentRouter.post("/delete/:commentId",auth, async(req, res) => {
+  const comment = {
+    _id: req.params.commentId,
+    text:req.body.text,
+    owner: req.body.owner,
+    ownerId:req.body.ownerId
+  }
+  try{
+    const post = await Post.findByIdAndUpdate(req.body.postId,{$pull: {
+            comments: comment
+          }
+      }, {
+          new: true
+      })
+        .exec((err, result) => {
+        if (err) {
+          return res.status(422).json({ error: err, msg: "Please provide correct owner name, owerId and postid"})
+        } else {
+            res.json(result)
+        }
+    })
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+
 
 export default commentRouter;
